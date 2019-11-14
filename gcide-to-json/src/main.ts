098d7c75cbbdc7c64a0ec6d65be087cfa85b6349
Jay Harris <jay.harris@outlook.co.nz>
2019-11-13 22:28:06 -0800

    Begins work on the converter

---
+++ gcide-to-json/src/main.ts
@@ -0,0 +1,13 @@
+import * as fs from 'fs';
+import { JSDOM } from 'jsdom';
+
+const inputFile = "../gcide/CIDE.A";
+
+const loadFileToDom = (fileName: string) => {
+    const text = fs.readFileSync(fileName);
+    const dom = new JSDOM(text); 
+    return dom.window.document;
+}
+
+const paragraphs = loadFileToDom(inputFile).querySelectorAll('p');
+console.log(paragraphs.length);
