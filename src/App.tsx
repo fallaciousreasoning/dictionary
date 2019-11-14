2627db016ee6c68376816c0a870017e4089560a9
Jay Harris <jay.harris@outlook.co.nz>
2019-11-14 15:44:33 -0800

    Removes reacty stuff

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,8 @@ import {Search } from './Search';
 
 function App() {
   return (
-    <div className="App">
+    <div className="dictionary">
       <Search/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
     </div>
   );
 }
