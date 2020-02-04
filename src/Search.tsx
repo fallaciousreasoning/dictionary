c738d87f44135bfa14447221d0e1d0b839204ca8
Jay Harris <jay.harris@outlook.co.nz>
2020-02-04 16:32:04 +1100

    Cleans up Search

--- src/Search.tsx
+++ src/Search.tsx
@@ -4,12 +4,15 @@ import { SearchResult } from './SearchResult';
 
 export const Search = (props) => {
     const [query, setQuery] = useState('');
+    const [loading, setLoading] = useState(true);
 
-    const [results, setResults] = useState<Entry[] | undefined>(undefined);
+    const [results, setResults] = useState<Entry[]>([]);
     const onSearchChanged = useCallback(async e => {
         const query: string = e.target.value;
         setQuery(query);
-        setResults(await findByRegex(query))
+        setLoading(true);
+        setResults(await findByRegex(query));
+        setLoading(false);
         window.history.replaceState({},
             `Dictionary results for ${query}`,
             `?query=${encodeURIComponent(query)}`)
@@ -20,8 +23,8 @@ export const Search = (props) => {
         const paramsQuery = decodeURIComponent(searchParams.get('query') || '');
 
         setQuery(paramsQuery);
-        findByRegex(paramsQuery).then(setResults);
-        return () => {}
+        findByRegex(paramsQuery).then(setResults).then(() => setLoading(false));
+        return () => { }
     }, []);
 
     return <div>
@@ -33,10 +36,10 @@ export const Search = (props) => {
                 onChange={onSearchChanged} />
         </div>
         <div>
-            {results === undefined ? <div className="spinner"/>
-                : (results.length === 0
-                    ? <h2>No results</h2>
-                    : results.map(entry => <SearchResult entry={entry} key={entry.word}/>))}
+            {(!loading && results.length === 0
+                ? <h2>No results</h2>
+                : results.map(entry => <SearchResult entry={entry} key={entry.word} />))}
+            {loading && <div className="spinner" />}
         </div>
     </div>;
 }
