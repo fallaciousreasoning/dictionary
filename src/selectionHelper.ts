8623fd444aa88dbe5da86b9b10aab6bcd68c96dd
Jay Harris <jay.harris@outlook.co.nz>
2020-02-05 15:45:39 +1100

    Update popovers

--- src/selectionHelper.ts
+++ src/selectionHelper.ts
@@ -18,7 +18,7 @@ export const getSelectedWord = () => {
         return null;
 
     if (selection.rangeCount === 0)
-      return null;
+        return null;
 
     const selectionText = selection.toString();
 
@@ -28,7 +28,7 @@ export const getSelectedWord = () => {
 
     const word = words[0].trim();
     if (word.length === 0)
-      return null;
+        return null;
 
     const range = selection.getRangeAt(0);
     return { word: word, rect: range.getBoundingClientRect() };
@@ -38,7 +38,7 @@ let selected: { word: string, rect: ClientRect };
 const selectedListeners: any[] = [];
 
 export const useSelectedWord = () => {
-    const [ r, setR ] = useState<typeof selected | null>(null);
+    const [r, setR] = useState<typeof selected | null>(null);
 
     useEffect(() => {
         selectedListeners.push(setR);
@@ -48,15 +48,25 @@ export const useSelectedWord = () => {
             if (removeAt === -1) return;
 
             selectedListeners.splice(removeAt, 1);
-        } 
+        }
     });
 
     return r;
 }
 
-document.addEventListener('selectionchange', () => {
+const clearSelection = () => {
+    for (const listener of selectedListeners)
+        listener(null);
+}
+
+const updateSelection = () => {
     const selected = getSelectedWord();
-    for (const listener of selectedListeners) {
+    for (const listener of selectedListeners)
         listener(selected);
-    }
-})
+}
+
+window.addEventListener('scroll', updateSelection, { passive: true });
+window.addEventListener('pushState', updateSelection);
+window.addEventListener('replaceState', updateSelection);
+
+document.addEventListener('selectionchange', updateSelection);
