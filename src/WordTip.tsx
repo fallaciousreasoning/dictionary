1c13bc2f52b162b512bc78c14b3b827b04bd1e86
Jay Harris <jay.harris@outlook.co.nz>
2020-02-07 13:32:25 +1100

    Word tip styles

--- src/WordTip.tsx
+++ src/WordTip.tsx
@@ -27,13 +27,13 @@ export const WordTip = ({ word }: { word: string }) => {
     const searchCallback = useCallback(() =>
         window.history.pushState(null, "Dictionary", `?query=${encodeURIComponent(selected ? selected.word : '')}`),
         [selected]);
-        
+
     if (!selected)
       return null;
 
     const virtualRef = new VirtualReference(selected.rect);
     return <Popper referenceElement={virtualRef} placement="bottom">
-        {({ ref, style, placement }) => <div ref={ref} style={style} data-placement={placement}>
+        {({ ref, style, placement }) => <div ref={ref} style={style} data-placement={placement} className="word-tip">
             <button onClick={searchCallback}>
                 Search
             </button>
